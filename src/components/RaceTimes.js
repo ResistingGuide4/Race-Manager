807b1bfad87eca96cee0a2707abe65ca3d5b0f5f
ResistingGuide4 <150295925+ResistingGuide4@users.noreply.github.com>
2023-12-29 02:38:09 +0000

    Adding clear last time and clear all times buttons

--- src/components/RaceTimes.js
+++ src/components/RaceTimes.js
@@ -1,28 +1,47 @@
 import { useState } from "react";
-function RaceTimes( {hours, minutes, seconds, milliseconds} ) {
-
+function RaceTimes({ hours, minutes, seconds, milliseconds }) {
   const [times, setTimes] = useState([]);
+
+  function clearLastTime() {
+    setTimes(times.slice(0, times.length - 1));
+  }
+
+  function clearAllTimes() {
+    setTimes([]);
+  }
+  
   function recordTime() {
-      const insertAt = times.length; // Could be any index
-      const nextTimes = [
-        // Items before the insertion point:
-        ...times.slice(0, insertAt),
-        // New item:
-          `${hours}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}:${milliseconds.toString().padStart(2, "0")}`,
-        // Items after the insertion point:
-        ...times.slice(insertAt)
-      ];
-      setTimes(nextTimes);
+    const insertAt = times.length; // Could be any index
+    const nextTimes = [
+      // Items before the insertion point:
+      ...times.slice(0, insertAt),
+      // New item:
+      `${hours}:${minutes.toString().padStart(2, "0")}:${seconds
+        .toString()
+        .padStart(2, "0")}:${milliseconds.toString().padStart(2, "0")}`,
+      // Items after the insertion point:
+      ...times.slice(insertAt),
+    ];
+    setTimes(nextTimes);
   }
   return (
+    <>
       <>
-          <button onClick={recordTime}>Record</button>
-          <h2>Times</h2>
-          <div>{times.map((record, recordIndex) => {
-            return <div key={recordIndex}><b>{recordIndex+1}: </b>{record}</div>
-          })}</div>
+        <button onClick={recordTime}>Record</button><button onClick={clearLastTime}>Clear last time</button><button onClick={clearAllTimes}>Clear all times</button>
       </>
-  )
+      <h2>Times</h2>
+      <div>
+        {times.map((record, recordIndex) => {
+          return (
+            <div key={recordIndex}>
+              <b>{recordIndex + 1}: </b>
+              {record}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default RaceTimes;
--- src/pages/race-recorder.js
+++ src/pages/race-recorder.js
@@ -3,35 +3,42 @@ import { useState, useEffect } from 'react';
 import RaceTimes from '../components/RaceTimes';
 
 function RaceRecorder() {
-    const [currentTime, setTime] = useState(0);
-    const [isTimerRunning, setIsTimerRunning] = useState(false);
+		const [currentTime, setTime] = useState(0);
+		const [isTimerRunning, setIsTimerRunning] = useState(false);
+		let needResetTimes = false;
 
-    function startAndStop() {
-        setIsTimerRunning(!isTimerRunning);
-    }
+		function startAndStop() {
+				setIsTimerRunning(!isTimerRunning);
+		}
 
-    useEffect(() => {
-        let interval;
-        if (isTimerRunning) {interval = setInterval(() => setTime(currentTime + 1), 10)}
-        return () => clearInterval(interval);
-    }, [isTimerRunning, currentTime]);
+		function resetTimer() {
+				setTime(0);
+				setIsTimerRunning(false);
+		}
 
-    const hours = Math.floor(currentTime / 360000);
-    const minutes = Math.floor((currentTime % 360000) / 6000);
-    const seconds = Math.floor((currentTime % 6000) / 100);
-    const milliseconds = currentTime % 100;
-    return (
-        <>
-            <Head>
-                <title>Race Recorder</title>
-            </Head>
-            <h1 id='stopWatch'>{`${hours}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}:${milliseconds.toString().padStart(2, "0")}`}</h1>
-            <div>
-                {isTimerRunning ? <button onClick={startAndStop}>Stop</button> : <button onClick={startAndStop}>Start</button>}
-            </div>
-            <RaceTimes hours={hours} minutes={minutes} seconds={seconds} milliseconds={milliseconds} />
-        </>
-    );
+		useEffect(() => {
+				let interval;
+				if (isTimerRunning) {interval = setInterval(() => setTime(currentTime + 1), 10)}
+				return () => clearInterval(interval);
+		}, [isTimerRunning, currentTime]);
+
+		const hours = Math.floor(currentTime / 360000);
+		const minutes = Math.floor((currentTime % 360000) / 6000);
+		const seconds = Math.floor((currentTime % 6000) / 100);
+		const milliseconds = currentTime % 100;
+		return (
+				<>
+						<Head>
+								<title>Race Recorder</title>
+						</Head>
+						<h1 id='stopWatch'>{`${hours}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}:${milliseconds.toString().padStart(2, "0")}`}</h1>
+						<div>
+								{isTimerRunning ? <button onClick={startAndStop}>Stop</button> : <button onClick={startAndStop}>Start</button>}
+							<button onClick={resetTimer}>Reset</button>
+						</div>
+						<RaceTimes hours={hours} minutes={minutes} seconds={seconds} milliseconds={milliseconds} />
+				</>
+		);
 }
 
 
