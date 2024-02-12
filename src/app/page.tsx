93327f557e8c6aeeaa7200ec0127b14126465ab2
ResistingGuide4 <150295925+ResistingGuide4@users.noreply.github.com>
2024-02-12 13:05:29 +0000

    Add recorder link to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Link href="/race-recorder">Test</Link>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
